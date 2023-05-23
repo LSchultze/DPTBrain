2aec7a9f649723038784e03a14ef65e55c5efa01
Landon Schultze <Landon.Schultze@student.csulb.edu>
2023-05-23 21:12:26 +0200

    Starter File

---
+++ dptbrain/app/page.js
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className='bg-gray-800 min-h-screen'>
+      <p className='font-bold text-9xl text-white'>YOOOOOOOOOOOOOOOOOO</p>
+      <p className='text-white text-9xl'>Certified Cump Piss Repository</p>
+    </div>
+
+  )
+}
