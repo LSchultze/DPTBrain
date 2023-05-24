b6b03d3eeefbeffa506a9b9802cfffcab0675ed5
Landon Schultze <Landon.Schultze@student.csulb.edu>
2023-05-24 22:44:03 +0200

    Color change + background no longer shrinks

--- dptbrain/pages/components/Header.jsx
+++ dptbrain/pages/components/Header.jsx
@@ -6,24 +6,24 @@ import { BiBrain } from 'react-icons/Bi'
 const Header = () => {
     return (
        <div className='text-white p-6' >
-            <div className='text-center mt-10 min-w-0' >
+            <div className='text-center ' >
                 <h2 className='font-bold text-2xl '>Digital PT Brain </h2>
                 
-                <div className='flex justify-center mt-12 mb-12'>
-                    <BiBrain size={150}/>
+                <div className='flex justify-center'>
+                    <BiBrain className="fill-green-500" size={150}/>
                 </div>
                 <div className='flex items-center justify-center mr-8'>
                     <h2 className='text-center'>Nathaniel Giovanni Yomogida, SPT</h2>
                     <h2 className='text-4xl ml-4'>/</h2>
                     <h2 className='ml-4 text-center'>Landon Kenneth Schultze, BSCS</h2>
                 </div>
-                <div className='mt-6'> 
+                <div className=''> 
                     <h2 className='font-bold text-2xl '>SDSU </h2>
                     <h2 className='font-bold text-2xl '>CSULB</h2>
                 </div>
                 <div className="flex items-center justify-center">
                     <div className='w-1/3 p-12'>    
-                        <Link href = '/' className="bg-white bg-opacity-10 hover:bg-gray-600 flex justify-center items-center lg:p-4 p-2 rounded-lg cursor-pointer ">
+                        <Link href = '/' className="bg-green-500 hover:bg-gray-600 flex justify-center items-center lg:p-4 p-2 rounded-lg cursor-pointer ">
                             <p className="font-bold">Begin</p>
                         </Link>
                     </div>
@@ -32,7 +32,7 @@ const Header = () => {
             
             </div>
 
-                <div className='w-9/12'>
+                <div className='grid grid-cols- gap-4'>
                     <h2 className={style.header}>Preface</h2>
                     <div>
                         <p className={style.paragraph}>This is a compilation of notes from the articles, 
@@ -52,7 +52,12 @@ const Header = () => {
                         which contains more bookmarks.</p>
 
                     </div>
-                    
+
+                </div>
+                <div className='p-10'>
+                    <ul class="list-disc">
+                        <li className='italic text-base'>General Terms</li>
+                    </ul>
                 </div>
             
        </div>
--- dptbrain/pages/index.js
+++ dptbrain/pages/index.js
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className='bg-custom_background h-screen w-screen'>
+    <main className='bg-custom_background flex flex-shrink-0 w-screen h-screen'>
       <Header/>
     </main>
   )
